98e4994fc0e6efdb8d069680cde1d5166bbf73e6
Atha Tsaqif <62295481+atha3417@users.noreply.github.com>
2025-03-13 22:35:47 +0700

    Update Sponshorship.tsx

--- src/Layouts/Sponshorship.tsx
+++ src/Layouts/Sponshorship.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sponsors from "../Componets/Elements/Sponsor/SPonsor"
+import Sponsors from "../Componets/Elements/Sponsor/Sponsor"
 
 
 const Sponshorship: React.FC = () => {
